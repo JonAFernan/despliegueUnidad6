535a9ce7eb0b3ee360bacb56b58792de180d9484
Jon A.Fernández <106017116+JonAFernan@users.noreply.github.com>
2024-12-15 17:57:48 +0100

    Update parImpar.py
    test webhooks

--- parImpar.py
+++ parImpar.py
@@ -2,3 +2,4 @@
 number = int(input("Introduce un numero entero: ")) #solicitamos un numero entero al usuario
 # imprimimos el resultado en base al resultado del operador ternario
 print (f"El número {number} es par" if number%2 == 0 else f"El número {number} es impar")
+#Generar commit
